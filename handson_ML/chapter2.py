66c010f52857e8361b33b0624056680bf65781b9
yumioh <iove0103@naver.com>
2025-07-08 17:40:38 +0900

    chapter 2

--- handson_ML/chapter2.py
+++ handson_ML/chapter2.py
@@ -2,6 +2,12 @@ from pathlib import Path
 import pandas as pd
 import tarfile
 import urllib.request
+import numpy as np
+from zlib import crc32
+
+import matplotlib.pyplot as plt
+
+
 
 def load_housing_data():
     # 기준 경로 설정 (여기서 모든 작업 수행)
@@ -26,3 +32,30 @@ def load_housing_data():
 housing = load_housing_data()
 
 print(housing) 
+# 어떤 카테고리가 있고 각 카테고리마다 얼마나 많은 구역이 있는지 확인
+print(housing["ocean_proximity"].value_counts())
+
+housing.hist(bins=50, figsize=(12,8))
+plt.show()
+
+# 테스트 세트 생성
+def shuffle_and_split_data(data, test_ratio):
+    shuffle_indices = np.random.permutation(len(data)) # 난수 생성
+    test_set_size = int(len(data) * test_ratio) 
+    test_indices = shuffle_indices[:test_set_size]
+    train_indices = shuffle_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+train_set, test_set = shuffle_and_split_data(housing, 0.2)
+print(len(train_set))
+print(len(test_set))
+
+def is_id_in_test_set(identifier, test_ratio):
+    # crc32 해시 함수로 적용
+    # 그 해시 값이 전체 가능한 해시값(2^32) 중 test ratio 비율 안에 들어오면 테스트 세트에 포함 => 항상 같은 결과 값을 줌
+    return crc32(np.int64(identifier)) < test_ratio * 2**32 
+
+def split_data_with_id_hash(data, test_ratio, id_column) :
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_ : is_id_in_test_set(id, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
