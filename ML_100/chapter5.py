11b817ddb7f7308957b1456df6bac6098f9e0aa4
yumioh <iove0103@naver.com>
2025-08-19 17:12:26 +0900

    chapter5

--- ML_100/chapter5.py
+++ ML_100/chapter5.py
@@ -4,6 +4,8 @@ import datetime
 import openpyxl
 from openpyxl.utils.dataframe import dataframe_to_rows
 from openpyxl.styles import PatternFill, Border, Side, Font
+from IPython.display import display, clear_output
+from ipywidgets import DatePicker
 
 m_store = pd.read_csv("d:\\data_analysis_lab\\ML_100\\data\\99_master\\m_store.csv")
 m_area = pd.read_csv("d:\\data_analysis_lab\\ML_100\\data\\99_master\\m_area.csv")
@@ -12,8 +14,8 @@ tbl_order_5 = pd.read_csv("./ML_100/data/0_input/tbl_order_202105.csv")
 tbl_order_6 = pd.read_csv("./ML_100/data/0_input/tbl_order_202106.csv")
 tbl_order_7 = pd.read_csv("./ML_100/data/0_input/tbl_order_202107.csv")
 
-print(m_store.head())
-print(tbl_order_7.head())
+#print(m_store.head())
+#print(tbl_order_7.head())
 
 # 기본 폴더 만들기 
 
@@ -61,9 +63,9 @@ def calc_delta(t) :
 
 def init_tran_df(trg_df) : 
     # 유지 보수용 매장 데이터 삭제 
-    trg_df = trg_df.loc[trg_df['store_id'] != 99]
+    trg_df = trg_df.loc[trg_df['store_id'] != 999]
     trg_df = pd.merge(trg_df, m_store, on='store_id', how='left')
-    trg_df = pd.merge(trg_df, m_store, on='area_cd', how='left')
+    trg_df = pd.merge(trg_df, m_area, on='area_cd', how='left')
     
     # 마스터 데이터에 없는 코드 대응 문자열 설정 
     trg_df.loc[trg_df['takeout_flag'] == 0, 'takeout_name'] = 'delivery'
@@ -72,7 +74,7 @@ def init_tran_df(trg_df) :
     trg_df.loc[trg_df['status'] == 0, 'status_name'] = '주문접수'
     trg_df.loc[trg_df['status'] == 1, 'status_name'] = '지불완료'
     trg_df.loc[trg_df['status'] == 2, 'status_name'] = '배달완료'
-    trg_df.loc[trg_df['status'] == 3, 'status_name'] = '주문취소'
+    trg_df.loc[trg_df['status'] == 9, 'status_name'] = '주문취소'
     
     trg_df.loc[:, 'order_date'] = pd.to_datetime(trg_df['order_accept_date']).dt.date
     
@@ -85,11 +87,13 @@ def init_tran_df(trg_df) :
 
 # 해당 월 데이터 초기화
 target_data = init_tran_df(target_data)
+print("============= 해당 월 데이터 초기화 ==============")
 print(target_data.head())
+print("============= 해당 월 데이터 초기화 ==============")
 
 def get_rank_df(target_data) : 
     #매잘 데이터 작성, 순위 DF 변환
-    tmp = target_data.loc[target_data['status']].isin([1.2])
+    tmp = target_data.loc[target_data['status'].isin([1,2])]
     rank = tmp.groupby(['store_id'])['total_amount'].sum().sort_values(ascending=False)
     rank = pd.merge(rank, m_store, on='store_id', how='left')
     return rank
@@ -97,8 +101,8 @@ def get_rank_df(target_data) :
 def get_cancel_rank_df(target_data) : 
     # 주문 취소율 계산, 순위 DF 반환
     cancel_df = pd.DataFrame()
-    cancel_cnt = target_data.loc[target_data['status'] == 9].groupby(['store_id'])['stroe_id'].count()
-    order_cnt = target_data.loc[target_data['status'].isin([1,2,9])].grouby(['store_id'])['store_id'].count()
+    cancel_cnt = target_data.loc[target_data['status'] == 9].groupby(['store_id'])['store_id'].count()
+    order_cnt = target_data.loc[target_data['status'].isin([1,2,9])].groupby(['store_id'])['store_id'].count()
     cancel_rate = (cancel_cnt / order_cnt) * 100
     cancel_df['cancel_rate'] = cancel_rate
     cancel_df = pd.merge(cancel_df, m_store, on='store_id', how='left')
@@ -193,8 +197,53 @@ def get_delivery_rank_store(target_id, target_df) :
     store_delivery_rank = delivery_rank.loc[delivery_rank['store_id'] == target_id].index + 1
     return store_delivery_rank[0]
 
+def make_report_hq_r2(target_date_list, output_folder) : 
+    wb = openpyxl.Workbook()
+    
+    file_date = ''
 
-def make_report_store(target_data, target_id, output_folder) : 
+    for tmp in target_date_list : 
+        df = pd.DataFrame(tmp)
+        
+        df_date = pd.to_datetime(df['order_accept_date']).max()
+        trg_date = df_date.strftime("%Y%m")
+        
+        if file_date ==  "":
+            # 처음에만 파일허용
+            file_date = trg_date
+        
+        rank = get_rank_df(df)
+        cancel_rank = get_cancel_rank_df(df)
+        
+        ws = wb.create_sheet(title=f'{trg_date}월분')
+        
+        cell = ws.cell(1,1)
+        cell.value = f'본부용 {trg_date}월분 요약 보고서'
+        cell.font = Font(bold=True, color="008080", size=20)
+        
+        cell = ws.cell(3,2)
+        cell.value = f'{max_str_date}월분 매출 총액'
+        cell.font = Font(bold=True, color="008080", size=20)
+        
+        cell = ws.cell(3,6)
+        cell.value = f'{'{:,}'.format(rank['total_amount'].sum())}'
+        cell.font = Font(bold=True, color="008080", size=20)
+        
+        cell = ws.cell(5,2)
+        cell.value = f'매출순위'
+        cell.font = Font(bold=True, color='0008080', size=16)
+        
+        data_export(rank, ws, 6, 2)
+        
+        # 주문 취소율 순위를 직접 출력
+        cell = ws.cell(5,8)
+        cell.value = f'주문 취소율 순위'
+        cell.font = Font(bold=True, color='008080', size=16)
+        
+        data_export(cancel_rank, ws, 6,8)
+    
+
+def make_report_store_r2(target_data, target_id, output_folder) : 
     rank = get_store_rank(target_id, target_data)
     sale = get_store_sale(target_id, target_data)
     cancel_rank = get_store_cancel_rank(target_id, target_data)
@@ -258,15 +307,15 @@ def make_report_store(target_data, target_id, output_folder) :
     ave_time = delivery_df.loc[delivery_df['store_id'] == target_id]['delta'].values[0]
     cell = ws.cell(5,14)
     cell.value = f'배달완료 소요 시간 순위'
-    cell.font = Font(blod=True, color="008080", size=16)
+    cell.font = Font(bold=True, color="008080", size=16)
 
     cell = ws.cell(5,18)
     cell.value = f'{delivery_rank}위, 평균{ave_time}분'
-    cell.font = Font(blod=True, color="008080", size=16)
+    cell.font = Font(bold=True, color="008080", size=16)
 
     cell = ws.cell(6,14)
     cell.value = f'각 매장 배달 시간 순위'
-    cell.font = Font(blod=True, color="008080", size=16)
+    cell.font = Font(bold=True, color="008080", size=16)
     
     data_export(delivery_df, ws, 7, 14)
     
@@ -274,4 +323,66 @@ def make_report_store(target_data, target_id, output_folder) :
     wb.close()
     
     
-    # P141
+# 본부용 보고서
+make_report_hq(target_data, output_dir)
+
+# 각 매장용 보고서
+for store_id in m_store.loc[m_store['store_id'] != 999]['store_id'] :
+    make_report_store_r2(target_data, store_id, output_dir)
+      
+      
+def make_active_folder(targetYM) :
+    now = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    target_output_dir_name = targetYM + "_" + now
+    target_output_dir = os.path.join(output_dir, target_output_dir_name)
+    os.makedirs(target_output_dir)
+    print(target_output_dir_name)
+    return target_output_dir
+
+target_output_dir = make_active_folder(tg_ym)
+
+# 본부용 보고서(생성 위치 변경)
+make_report_hq_r2(target_data, target_output_dir)
+
+for store_id in m_store.loc[m_store['store_id'] != 999]['store_id'] :
+    area_cd = m_store.loc[m_store['store_id'] == store_id]['area_cd']
+    area_name = m_area.loc[m_area['area_cd'] == area_cd.values[0]]['narrow_area'].values[0]
+    target_store_output_dir = os.path.join(target_output_dir, area_name)
+    os.makedirs(target_store_output_dir, exist_ok=True)
+    make_report_store(target_data, store_id, target_store_output_dir) 
+    
+def order_by_date(val) : 
+    clear_output()
+    display(date_picker)
+    
+    df_array = []
+    
+    print("데이터 확인. 데이터를 준비합니다")
+    
+    date_str = str(val['new'])
+    date_dt = datetime.datetime.strptime(date_str, '%Y-%m-%d')
+    target_ym = date_dt.strftime('%Y%m')
+    
+    # 폴더 자동 생성
+    target_output_dir = make_active_folder(target_ym)
+    
+    # 선택한 기준 월 데이터 확인
+    target_file = 'tbl_order_' + target_ym + '.csv'
+    if os.path.exists(os.path.join(input_dir, target_file)) == False :
+        print(f'{target_file}이 없습니다')
+        return
+    else : 
+        df = pd.read_csv(os.path.join(input_dir, target_file))
+        df = init_tran_df(df)
+        df_array.append(df)
+        
+    target_ym_old = str(int(target_ym) - 1)
+    target_file = 'tbl_order' + target_ym_old + ".csv"
+    if os.path.exists(os.path.join(input_dir, target_file)) == True:
+        df = pd.read_csv(os.path.join(input_dir, target_file))
+        df = init_tran_df(df)
+        df_array.append(df)
+    
+    print("데이터 분비 완료. 보고서를 생성합니다...")
+    
+    make_report_hq_r2()
